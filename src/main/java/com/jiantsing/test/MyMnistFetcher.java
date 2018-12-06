2e7f7ba17da0a5402419ad0b43b00b2b087b9202
shootercheng <3281328128@qq.com>
2018-12-06 15:59:20 +0800

    Create MyMnistFetcher.java
    window系统下载的*.gz解压后之后的文件名和MnistFetcher提供常量名不同

---
+++ src/main/java/com/jiantsing/test/MyMnistFetcher.java
@@ -0,0 +1,10 @@
+package com.jiantsing.test;
+
+import org.datavec.image.mnist.MnistFetcher;
+
+public class MyMnistFetcher extends MnistFetcher{
+	public static final String trainingFilesFilename_unzipped = "train-images.idx3-ubyte";
+	public static final String trainingFileLabelsFilename_unzipped = "train-labels.idx1-ubyte";
+	public static final String testFilesFilename_unzipped = "t10k-images.idx3-ubyte";
+	public static final String testFileLabelsFilename_unzipped = "t10k-labels.idx1-ubyte";
+}
